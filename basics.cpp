31ab0750ec9da0f68f237cbfa76dec3f32965394
Ulises <ulisesfonseca35@gmail.com>
2020-11-05 16:19:22 -0600

    While loop

--- basics.cpp
+++ basics.cpp
@@ -2,10 +2,17 @@
 
 int main()
 {
+    int b=0;
     std::cout<<"Foor loop..."<<std::endl;
     for (int i = 1; i <= 5; i++)
     {
         std::cout<<i<<std::endl;
+    }
+    std::cout<<"While loop..."<<std::endl;
+    while(b<3)
+    {
+        std::cout<<"Miriam"<<std::endl;
+        b++;
     }
     
     return 0;
